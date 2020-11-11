471f7ddeba9e470dbd12f9e4f7b19d0aed593bc7
luna bartolozzi <luna.bartolozzi@gmail.com>
2020-11-10 22:26:00 -0300

    primer actualizacion

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+const precios = [
+  100,
+  200,
+  300,
+  400,
+  500,
+  600,
+  700,
+  800,
+  100,
+  100,
+  100,
+  100,
+  100,
+  100,
+  100,
+  100,
+  100,
+  100,
+];
+
+const presupuesto = {
+  precioTotal: 0,
+};
+
+function obtenerPresupuesto() {
+  var nombreProyecto = prompt("Ingrese el nombre de su proyecto");
+  alert("Gracias por el pedido para " + nombreProyecto);
+
+  var checkboxes = document.getElementsByName("checkbox");
+  var posicion = 0;
+  for (var checkbox of checkboxes) {
+    if (checkbox.checked) {
+      presupuesto.precioTotal = presupuesto.precioTotal + precios[posicion];
+    }
+    posicion++;
+  }
+  document.getElementById("precio").innerHTML = presupuesto.precioTotal;
+  if (sessionStorage.email != undefined) {
+    document.getElementById("datos").innerHTML =
+      "Email: " + sessionStorage.email;
+  } else {
+    document.getElementById("datos").innerHTML = "No has introducido tu email";
+  }
+}
+
+function guardarDatos() {
+  sessionStorage.email = document.getElementById("email").value;
+}
